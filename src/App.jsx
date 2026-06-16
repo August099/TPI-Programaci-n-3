fd6540af6e77ac6f3fcb72960141cee89262fd7e
Augusto Roncaglia <augustoroncaglia9@gmail.com>
2026-06-16 09:15:29 -0300

    solucion de conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,36 @@ import Carrusel from './components/store/carrusel/carrusel.jsx'
 import Card from './components/store/card/card.jsx'
 import Login from './components/Auth/login/login.jsx'
 import Contacto from './components/contactos/Contacto/Contacto.jsx'
+import Protected from './components/routing/Protected.jsx';
+import { useState } from 'react';
+
 
 function App() {
+
+
+  const [IsLoggedIn,SetIsLoggedIn]=useState(false)
+
+  const handleLogin = () =>{
+    SetIsLoggedIn(true)
+    
+  }
+
+  
 
   return (
   <div>
 
      <BrowserRouter>
 
+      // prueba
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/Store" element={<Card />} />
+        <Route path="/login" element={<Login onHandleLogin={handleLogin}/>} />
+        <Route element={<Protected IsSignedIn={IsLoggedIn}/>}>
+          <Route path="/Store" element={<Card />}/>
+        </Route>
+
       </Routes>
       
-      <Navigate to="/login" />
 
     </BrowserRouter>
 
